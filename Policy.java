6644f3f2ed91565c8d8898a3d30ce53afb12aeba
MusabFiraz <144862897+MusabFiraz@users.noreply.github.com>
2023-09-13 16:39:06 -0400

    Policy
    Added code for Policy class.

---
+++ Policy.java
@@ -0,0 +1,126 @@
+public class Policy {
+    private String policyNumber;
+    private String providerName;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String smokingStatus;
+    private int height;
+    private int weight;
+
+    // Constructor with default values
+    public Policy() {
+        this.policyNumber = "";
+        this.providerName = "";
+        this.firstName = "";
+        this.lastName = "";
+        this.age = 0;
+        this.smokingStatus = "";
+        this.height = 0;
+        this.weight = 0;
+    }
+
+    // Constructor with arguments
+    public Policy(String policyNumber, String providerName, String firstName, String lastName, int age,
+                  String smokingStatus, int height, int weight) {
+        this.policyNumber = policyNumber;
+        this.providerName = providerName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.smokingStatus = smokingStatus;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    // Getters and setters
+    public String getPolicyNumber() {
+        return policyNumber;
+    }
+
+    public void setPolicyNumber(String policyNumber) {
+        this.policyNumber = policyNumber;
+    }
+
+    public String getProviderName() {
+        return providerName;
+    }
+
+    public void setProviderName(String providerName) {
+        this.providerName = providerName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSmokingStatus() {
+        return smokingStatus;
+    }
+
+    public void setSmokingStatus(String smokingStatus) {
+        this.smokingStatus = smokingStatus;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    // Method to calculate BMI
+    public double calculateBMI() {
+        return (weight * 703.0) / (height * height);
+    }
+
+    // Method to calculate insurance policy price
+    public double calculatePolicyPrice() {
+        double baseFee = 600.0;
+        double additionalFee = 0.0;
+
+        if (age > 50) {
+            additionalFee += 75.0;
+        }
+
+        if (smokingStatus.equals("smoker")) {
+            additionalFee += 100.0;
+        }
+
+        double bmi = calculateBMI();
+        if (bmi > 35) {
+            additionalFee += (bmi - 35) * 20.0;
+        }
+
+        return baseFee + additionalFee;
+    }
+}
